c78796ed0839c87f3ce8b45900b1671b903367e6
Gabriel Potje <gpotje.souza@gmail.com>
2020-04-18 02:26:17 -0300

    Little improvement in board printing

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -35,9 +35,19 @@ public class ChessMatch {
 		//board.placePiece(new Rook(board, Color.WHITE),new  Position(2, 1));
 		//board.placePiece(new king(board, Color.BLACK),new  Position(0, 4));
 		//board.placePiece(new king(board, Color.WHITE),new  Position(7, 4));
-		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
-		placeNewPiece('e', 8, new Rook(board, Color.BLACK));
-		placeNewPiece('e', 1, new king(board, Color.WHITE));
+		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new king(board, Color.WHITE));
+
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new king(board, Color.BLACK));
 	}
 	
 	
